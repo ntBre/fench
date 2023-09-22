a70500a713cf3764898f8839594a3c532e847b77
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-22 15:55:41 -0400

    delete db if it already exists

--- try.py
+++ try.py
@@ -1,12 +1,17 @@
+import os
+
 from ibstore._store import MoleculeStore
 from openff.qcsubmit.results import OptimizationResultCollection
 
-data = "try"
+db_name = "try.sqlite"
+if os.path.exists(db_name):
+    os.remove(db_name)
+
 store = MoleculeStore.from_qcsubmit_collection(
     OptimizationResultCollection.parse_file(
         "testfiles/filtered-core-opt.json",
     ),
-    database_name=f"{data}.sqlite",
+    database_name=db_name,
 )
 
 ff = "openff-2.1.0"
