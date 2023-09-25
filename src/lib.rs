938b2784ce6c54b921f2ad87ac8e0680b1e92827
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-25 15:02:40 -0400

    add approx and test values of ddes
    catch a unit issue in setting the positions of atoms in openmm. set_positions
    was previously expecting units of angstroms but I was passing bohr
    
    epsilon is still quite hight at 0.63. maybe this issue is cropping up elsewhere?

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,40 @@ mod utils {
 
 #[cfg(test)]
 mod tests {
+    use std::fs::read_to_string;
+
+    use approx::assert_abs_diff_eq;
+
     use crate::bench::MoleculeStore;
 
     #[test]
     fn get_dde() {
         let store = MoleculeStore::from_json("testfiles/store.json").unwrap();
         let ff = "openff-2.1.0.offxml";
-        let res = store.get_dde(ff);
-        // this is the number from Python
-        assert_eq!(res.len(), 217);
+        let got = store.get_dde(ff);
+        let s = read_to_string("testfiles/dde.txt").unwrap();
+        let want: Vec<_> = s
+            .lines()
+            .map(|line| {
+                let fields: Vec<_> = line.split_ascii_whitespace().collect();
+                (fields[0].to_owned(), fields[1].parse::<f64>().unwrap())
+            })
+            .collect();
+        assert_eq!(got.len(), want.len());
+        let (gr, gv): (Vec<_>, Vec<_>) = got.into_iter().unzip();
+        let (wr, wv): (Vec<_>, Vec<_>) = want.into_iter().unzip();
+        assert_eq!(gr, wr);
+        let eps = 0.63;
+        let mut disagree = 0;
+        for (i, (g, w)) in gv.iter().zip(wv.iter()).enumerate() {
+            if (g - w).abs() > eps {
+                println!("{i:5}{:>12}{g:12.8}{w:12.8}{:12.8}", gr[i], g - w);
+                disagree += 1;
+            }
+        }
+        if disagree > 0 {
+            println!("{} / {} disagree", disagree, gv.len());
+        }
+        assert_abs_diff_eq!(gv.as_slice(), wv.as_slice(), epsilon = eps);
     }
 }
