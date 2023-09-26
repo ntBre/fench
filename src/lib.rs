6c5590e302f7bfd16547594490e78b013480591e
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-26 10:05:56 -0400

    move utils to their own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,6 @@
 pub mod bench;
 
-mod utils {
-    /// compute the minimum of `v` and then subtract it from all of the elements
-    #[inline]
-    pub(crate) fn make_rel(v: &mut [f64]) {
-        let min = *v.iter().min_by(|x, y| x.total_cmp(y)).unwrap();
-        for e in v.iter_mut() {
-            *e -= min;
-        }
-    }
-}
+mod utils;
 
 #[cfg(test)]
 mod tests;
---
+++ src/utils.rs
@@ -0,0 +1,8 @@
+/// compute the minimum of `v` and then subtract it from all of the elements
+#[inline]
+pub(crate) fn make_rel(v: &mut [f64]) {
+    let min = *v.iter().min_by(|x, y| x.total_cmp(y)).unwrap();
+    for e in v.iter_mut() {
+        *e -= min;
+    }
+}
